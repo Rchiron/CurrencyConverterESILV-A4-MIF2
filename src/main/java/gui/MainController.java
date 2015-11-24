3737be779b2cc85c469cee0f66b0457b07ece74c
ChiChi <chironromain@gmail.com>
2015-11-24 23:56:00 +0100

    Ajout des principales devises

--- src/main/java/gui/MainController.java
+++ src/main/java/gui/MainController.java
@@ -17,11 +17,23 @@ public class MainController {
     }
 
     public void converterList(){
-        Currency eur = new Currency("EUR", 1.06514);
-        Currency dollars = new Currency("USD", 1);
+        Currency eur = new Currency("EUR - Euro", 1.06472);
+        Currency dollars = new Currency("USD - Dollar USA", 1);
+        Currency gpb = new Currency("GBP - Livre britannique", 1.50813);
+        Currency inr = new Currency("INR - Roupie indienne",0.01508);
+        Currency aud = new Currency("AUD - Dollar australien",0.72732);
+        Currency cad = new Currency("CAD - Dollar canadien",0.75174);
+        Currency zar = new Currency("ZAR - Rand sud-africain", 0.65612);
+        Currency jpy = new Currency("JPY - Yen japonais", 0.00816);
 
         addCurrency(eur);
         addCurrency(dollars);
+        addCurrency(gpb);
+        addCurrency(inr);
+        addCurrency(aud);
+        addCurrency(cad);
+        addCurrency(zar);
+        addCurrency(jpy);
     }
 
     public List<Currency> getList(){
